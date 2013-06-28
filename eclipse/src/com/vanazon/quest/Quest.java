717e9f9511643c6048bc2c47a8742c3cab1a2a85
mayronl <mayronl@SEA-1201012588.ant.amazon.com>
2013-06-28 15:05:26 -0700

    Hopefully this works now Commit Quest Tasks

--- eclipse/src/com/vanazon/quest/Quest.java
+++ eclipse/src/com/vanazon/quest/Quest.java
@@ -1,7 +1,14 @@
 package com.vanazon.quest;
 
+import java.util.HashMap;
+import java.util.List;
+
+import com.vanazon.entities.GameObject;
+
 public class Quest {
-	
-	
+	HashMap<GameObject,List<GameObject>> changeLoadOnUseObject;
+	HashMap<GameObject,List<GameObject>> changeUnLoadOnUseObject;
+	HashMap<GameObject,List<GameObject>> changeLoadOnUseBip;
+	HashMap<GameObject,List<GameObject>> changeUnLoadOnUseBip;
 
 }
