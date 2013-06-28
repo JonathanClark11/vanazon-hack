8c45c77a046710e53cf39b4ef66c17a2b728ff57
mayronl <mayronl@SEA-1201012588.ant.amazon.com>
2013-06-28 16:48:46 -0700

    Hopefully now I push the demofile

--- eclipse/src/com/vanazon/quest/Quest.java
+++ eclipse/src/com/vanazon/quest/Quest.java
@@ -1,7 +1,15 @@
 package com.vanazon.quest;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.List;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import android.util.Xml;
 
 import com.vanazon.entities.GameObject;
 
@@ -10,5 +18,19 @@ public class Quest {
 	HashMap<GameObject,List<GameObject>> changeUnLoadOnUseObject;
 	HashMap<GameObject,List<GameObject>> changeLoadOnUseBip;
 	HashMap<GameObject,List<GameObject>> changeUnLoadOnUseBip;
+	
 
+	public Quest (File f){
+		try {
+			XmlPullParser parser = Xml.newPullParser();
+			parser.setInput(new FileInputStream(f), "");
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (XmlPullParserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
