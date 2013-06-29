8a8b46bc7f6f30c6fe809681fa6f4379ddd7e929
mayronl <mayronl@SEA-1201012588.ant.amazon.com>
2013-06-28 17:46:34 -0700

    Finshed parsing for tasks. Updated document to final version

--- eclipse/src/com/vanazon/quest/Quest.java
+++ eclipse/src/com/vanazon/quest/Quest.java
@@ -3,6 +3,8 @@ package com.vanazon.quest;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -11,26 +13,182 @@ import org.xmlpull.v1.XmlPullParserException;
 
 import android.util.Xml;
 
-import com.vanazon.entities.GameObject;
-
 public class Quest {
-	HashMap<GameObject,List<GameObject>> changeLoadOnUseObject;
-	HashMap<GameObject,List<GameObject>> changeUnLoadOnUseObject;
-	HashMap<GameObject,List<GameObject>> changeLoadOnUseBip;
-	HashMap<GameObject,List<GameObject>> changeUnLoadOnUseBip;
+	HashMap<String,List<String>> changeLoadOnUseObject;
+	HashMap<String,List<String>> changeUnLoadOnUseObject;
+	HashMap<String,List<String>> changeLoadOnUseBip;
+	HashMap<String,List<String>> changeUnLoadOnUseBip;
 	
 
 	public Quest (File f){
+		changeLoadOnUseObject = new HashMap<String,List<String>>();
+		changeUnLoadOnUseObject = new HashMap<String,List<String>>();
+		changeLoadOnUseBip = new HashMap<String,List<String>>();
+		changeUnLoadOnUseBip = new HashMap<String,List<String>>();
+		FileInputStream fInput = null;
 		try {
+			fInput = new FileInputStream(f);
+		
 			XmlPullParser parser = Xml.newPullParser();
-			parser.setInput(new FileInputStream(f), "");
-
+			parser.setInput(fInput, "");
+			parser.nextTag();
+			readFeed(parser);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (XmlPullParserException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally{
+			try {
+				fInput.close();
+			} catch (IOException e) {
+			}
 		}
 	}
+
+	private static final String ns = null;
+	private void readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
+		parser.require(XmlPullParser.START_TAG, ns, "feed");
+	    while (parser.next() != XmlPullParser.END_TAG) {
+	        if (parser.getEventType() != XmlPullParser.START_TAG) {
+	            continue;
+	        }
+	        String name = parser.getName();
+	        // Starts by looking for the entry tag
+	        if (name.equals("entry")) {
+	            readEntry(parser);
+	        } else {
+	            skip(parser);
+	        }
+	    } 
+		
+	}
+	
+	private void readEntry(XmlPullParser parser) throws XmlPullParserException, IOException {
+	    parser.require(XmlPullParser.START_TAG, ns, "entry");
+	    String id = null;
+	    List<String> BL=null,BUL=null,OL = null,OUL = null;
+	    while (parser.next() != XmlPullParser.END_TAG) {
+	        if (parser.getEventType() != XmlPullParser.START_TAG) {
+	            continue;
+	        }
+	        String name = parser.getName();
+	        if (name.equals("id")) {
+	            id = readName(parser);
+	        } else if (name.equals("BipLoad")) {
+	            BL = readBL(parser);
+	        } else if (name.equals("BipUnLoad")) {
+	            BUL = readBUL(parser);
+	        }else if (name.equals("ObjectLoad")) {
+	            OL = readOL(parser);
+	        }else if (name.equals("ObjectUnLoad")) {
+	            OUL = readOUL(parser);
+	        } else {
+	            skip(parser);
+	        }
+	    }
+		changeLoadOnUseObject.put(id, OL);
+		changeUnLoadOnUseObject.put(id, OUL);
+		changeLoadOnUseBip.put(id, BL);
+		changeUnLoadOnUseBip.put(id, BUL);
+	}
+	private List<String> readOUL(XmlPullParser parser) throws XmlPullParserException, IOException {
+	    parser.require(XmlPullParser.START_TAG, ns, "ObjectUnLoad");
+	    List<String> l = new ArrayList<String>();
+	    while (parser.next() != XmlPullParser.END_TAG) {
+	        if (parser.getEventType() != XmlPullParser.START_TAG) {
+	            continue;
+	        }
+	        String name = parser.getName();
+	        if (name.equals("id")) {
+	            l.add(readName(parser));
+	        } else {
+	            skip(parser);
+	        }
+	    }
+	    return l;
+	}
+	private List<String> readOL(XmlPullParser parser) throws XmlPullParserException, IOException {
+	    parser.require(XmlPullParser.START_TAG, ns, "BipLoad");
+	    List<String> l = new ArrayList<String>();
+	    while (parser.next() != XmlPullParser.END_TAG) {
+	        if (parser.getEventType() != XmlPullParser.START_TAG) {
+	            continue;
+	        }
+	        String name = parser.getName();
+	        if (name.equals("id")) {
+	            l.add(readName(parser));
+	        } else {
+	            skip(parser);
+	        }
+	    }
+	    return l;
+	}
+	private List<String> readBUL(XmlPullParser parser) throws XmlPullParserException, IOException {
+	    parser.require(XmlPullParser.START_TAG, ns, "BipUnLoad");
+	    List<String> l = new ArrayList<String>();
+	    while (parser.next() != XmlPullParser.END_TAG) {
+	        if (parser.getEventType() != XmlPullParser.START_TAG) {
+	            continue;
+	        }
+	        String name = parser.getName();
+	        if (name.equals("id")) {
+	            l.add(readName(parser));
+	        } else {
+	            skip(parser);
+	        }
+	    }
+	    return l;
+	}
+	private List<String> readBL(XmlPullParser parser) throws XmlPullParserException, IOException {
+	    parser.require(XmlPullParser.START_TAG, ns, "BipLoad");
+	    List<String> l = new ArrayList<String>();
+	    while (parser.next() != XmlPullParser.END_TAG) {
+	        if (parser.getEventType() != XmlPullParser.START_TAG) {
+	            continue;
+	        }
+	        String name = parser.getName();
+	        if (name.equals("id")) {
+	            l.add(readName(parser));
+	        } else {
+	            skip(parser);
+	        }
+	    }
+	    return l;
+	}
+	private String readName(XmlPullParser parser) throws XmlPullParserException, IOException {
+	    parser.require(XmlPullParser.START_TAG, ns, "id");
+	    String title = readText(parser);
+	    parser.require(XmlPullParser.END_TAG, ns, "id");
+	    return title;
+	}
+	private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
+	    String result = "";
+	    if (parser.next() == XmlPullParser.TEXT) {
+	        result = parser.getText();
+	        parser.nextTag();
+	    }
+	    return result;
+	}
+	private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+	    if (parser.getEventType() != XmlPullParser.START_TAG) {
+	        throw new IllegalStateException();
+	    }
+	    int depth = 1;
+	    while (depth != 0) {
+	        switch (parser.next()) {
+	        case XmlPullParser.END_TAG:
+	            depth--;
+	            break;
+	        case XmlPullParser.START_TAG:
+	            depth++;
+	            break;
+	        }
+	    }
+	 }
 }
